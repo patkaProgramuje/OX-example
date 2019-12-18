694a7f5586a9028da02e022f40783e9ad35a6afa
Patrycja Hyjek <patrycja_hyjek@epam.com>
2019-12-18 11:07:54 +0100

    Added need tools to run project.

--- src/main/java/pl/patrycja/testproject/Main.java
+++ src/main/java/pl/patrycja/testproject/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int tab[][] = new int[1000][1000];
+        int[][] tab = new int[1000][1000];
 
         for (int i = 0; i < tab.length; i++) {
             for (int j = 0; j < tab.length; j++) {
