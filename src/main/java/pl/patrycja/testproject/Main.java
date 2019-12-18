b06f17a7fe1cf3889d243f88022abff8cd3ed425
Patrycja Hyjek <patrycja_hyjek@epam.com>
2019-12-18 10:46:58 +0100

    Created method which present twodimensional array with size equals 1000 to see that array with that size is not comfortable to play on it.

---
+++ src/main/java/pl/patrycja/testproject/Main.java
@@ -0,0 +1,17 @@
+package pl.patrycja.testproject;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int tab[][] = new int[1000][1000];
+
+        for (int i = 0; i < tab.length; i++) {
+            for (int j = 0; j < tab.length; j++) {
+                tab[i][j] = 0;
+                System.out.print(tab[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
